5e58984e35b40ba5de71a2181431b0eecc90c92e
dmoreno06 <mordiana1506@gmail.com>
2019-02-08 00:58:57 -0500

    tipos de variables

---
+++ conceptosBasicos/scripts.js
@@ -0,0 +1,10 @@
+let holaVida = "hola vida hoy voy a recibir lo mejor de ti.";
+
+function prueba(){
+    return 'hola a mi vida con tigo';
+}
+let _palabra,nodicha;
+let $amor;
+let porti;
+
+const anioNacimiento = 1990;
