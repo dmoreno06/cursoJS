1c1b2d6e5885a13f1d4e9e93fab57e92e9237617
dmoreno06 <mordiana1506@gmail.com>
2019-02-10 23:56:49 -0500

    tipos de variables

---
+++ conceptosBasicos/scripts/scripts.1.js
@@ -0,0 +1,15 @@
+//primitivos
+ let nombre ="hola Laura";
+ let registro = "bienvenido,"+nombre;
+ let otroRegistro = `BIENVENIDO, ${nombre}`;
+ console.log(registro);
+ //numero
+ let nacimiento = 1990;
+ let anio = new Date().getFullYear();
+ let edad = anio - nacimiento;
+
+ console.log('6'+6);
+
+  //boolean
+  true, false
+//complejos
