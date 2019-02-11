152aae660706b608c9e03712e2a2ade01a765477
dmoreno06 <mordiana1506@gmail.com>
2019-02-11 01:38:33 -0500

    propiedades

---
+++ conceptosBasicos/scripts/propiedades.js
@@ -0,0 +1,8 @@
+let hola = "hola vida";
+let hola1 = "hola luna";
+let hola2 = "hola viento";
+let hola3 = "hola montaña";
+let hola4 = "hola tormenta";
+console.log(hola.indexOf('v') == 0);
+console.log(hola1.indexOf('v') == 0);
+console.log(hola3.indexOf('v') == 0);
