056736e98b6483e26df832fc37b9b4688b71320c
dmoreno06 <mordiana1506@gmail.com>
2019-02-15 02:08:45 -0500

    arrays js

---
+++ estructuras_de_datos/arrays.js
@@ -0,0 +1,15 @@
+let arr = [luz,sueños,amor,ensenanza];
+// 0 1 2 3  indice
+/*console.log(arr.length);
+console.log(arr[0]);
+//contador de las pciciones de arredos
+let sum = 0;
+for (let index = 0; index < array.length; index++) {
+    //let element = arr[index];
+    let element = arr[index];
+    if(typeof element == 'number') continue;
+    //sum += arr[index];
+    sum += element;
+}
+Array.prototype.saludo = 'hola';
+console.log(arr.saludo);*/
