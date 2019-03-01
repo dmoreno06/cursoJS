315cdc4eb5b751ad34f88fdea63e62d934686047
DMoreno_06 <mordiana1506@gmail.com>
2019-03-01 01:06:09 -0500

    my first function in js

---
+++ funciones/scriptsFunciones/funcionesA.js
@@ -0,0 +1,28 @@
+// suma de dos numeros
+function sumar(n1,n2){
+    let resultado;
+    resultado = n1+n2;
+    console.log(resultado);
+}
+sumar(3,8);
+
+function sumarOtra(n1,n2){
+    if(n1>n2){
+        return n1+n2;
+    }
+    return n2;
+}
+let resultado1 = sumarOtra(1,8);
+console.log(resultado1);
+
+function resta(n1,n2){
+    if(n1>n2){
+        return {
+                n1: n1 - 1,
+                n2: n2 - 1
+                }
+    }
+    return n2;
+}
+let resultado2 = resta(10,8);
+console.log(resultado2);
